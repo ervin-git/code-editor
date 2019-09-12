9b3dce0e903d65acc8639eb387f5e2f71cca98e5
Armand Rosario <31975821+armandrosario@users.noreply.github.com>
2019-09-11 23:20:41 -0500

    Update Controller.java

--- src/main/java/application/Controller.java
+++ src/main/java/application/Controller.java
@@ -1,20 +1,73 @@
 package application;
 
-
+import java.awt.List;
+import java.beans.EventHandler;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
 
 public class Controller {
-	@FXML
-	public Label label;
-	public MenuItem menuItem;
 	
-	public void initialize() {
-		
-	}
-	@FXML
-	private void changeText(ActionEvent event) {
-		menuItem.setText("Change Text");
-	}
+	ArrayList<Project>projects= new ArrayList<Project>();
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private MenuItem close;
+
+    @FXML
+    private MenuItem create;
+
+    @FXML
+    private MenuItem open;
+
+    @FXML
+    private MenuItem save;
+    
+    @FXML
+    private TextField createText;
+    
+
+
+    @FXML
+    void close(ActionEvent event) {
+    }
+
+    @FXML
+    public void create(ActionEvent event) {
+    	create.setOnAction(e ->Create.display("Create"));
+ 
+    	
+    }
+
+    @FXML
+    void open(ActionEvent event) {
+    }
+
+    @FXML
+    void save(ActionEvent event) {
+    }
+
+    @FXML
+    void initialize() {
+        assert close != null : "fx:id=\"close\" was not injected: check your FXML file 'codeeditor.fxml'.";
+        assert create != null : "fx:id=\"create\" was not injected: check your FXML file 'codeeditor.fxml'.";
+        assert open != null : "fx:id=\"open\" was not injected: check your FXML file 'codeeditor.fxml'.";
+        assert save != null : "fx:id=\"save\" was not injected: check your FXML file 'codeeditor.fxml'.";
+        
+   
+
+
+    }
+
 }
+
