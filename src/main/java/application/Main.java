0fe5ba8b48f420fd6d506a91fe2ac98946c4abd7
Armand Rosario <31975821+armandrosario@users.noreply.github.com>
2019-09-12 11:38:51 -0500

    Update Main.java
    Added return scene function to assist in coding

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -7,11 +7,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class Main extends Application {
 
+	private static Stage stage;
     @Override
     public void start(Stage primaryStage) throws Exception {
+    	stage=primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("codeeditor.fxml"));
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
         int width = gd.getDisplayMode().getWidth();
@@ -20,10 +26,16 @@ public class Main extends Application {
         primaryStage.setTitle("Code Editor v1.0");
         primaryStage.setScene(scene);
         primaryStage.show();
+      
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+    public static Stage getStage() {
+    	return stage;
+    }
+   
+}
 }
