e4ca463d46a55569b040543babad6412d55b37e9
Ervin Canigur <ervin@canigur.com>
2019-09-12 18:38:31 -0500

    Cleaned up code
    Fixed tree view to show fill directory files

--- src/main/java/application/Controller.java
+++ src/main/java/application/Controller.java
@@ -1,43 +1,19 @@
 package application;
 
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
-import java.util.Scanner;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextArea;
-
-
-import java.awt.List;
-import javafx.stage.FileChooser;
-
-import java.beans.EventHandler;
-import java.io.*;
-import javafx.scene.Parent;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
-import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
+import javafx.scene.control.*;
 import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
-
-
-
-
-
-
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 public class Controller {
-	
-	ArrayList<Project>projects= new ArrayList<Project>();
-
     @FXML
     private ResourceBundle resources;
 
@@ -55,46 +31,54 @@ public class Controller {
 
     @FXML
     private MenuItem save;
-    
+
     @FXML
     private TextField createText;
-    
+
     @FXML
     private TextArea codeArea;
-    
+
     @FXML
-    private TreeView tree;
+    private TreeView<String> tree;
 
     @FXML
     void close(ActionEvent event) {
+        codeArea.clear();
+        tree.setRoot(null);
     }
 
     @FXML
     public void create(ActionEvent event) {
-    	create.setOnAction(e ->Create.display("Create"));
+        create.setOnAction(e -> Create.display("Create"));
     }
 
     @FXML
     void open(ActionEvent event) throws IOException {
-    	DirectoryChooser directoryChooser=new DirectoryChooser();
-    	File selectedDirectory = directoryChooser.showDialog(Main.getStage());
-    	String fileName=selectedDirectory.getName();
-    	String location=selectedDirectory.getAbsolutePath();
-    	//
-
-    	TreeItem<String> rootItem = new TreeItem<String> (fileName);
-        rootItem.setExpanded(true);
-        tree.setRoot(rootItem);
-        tree = new TreeView<String> (rootItem);    
-        System.out.println(tree.getRoot());		
-         
-         
-   
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File selectedDirectory = directoryChooser.showDialog(new Stage());
+        loadTree(selectedDirectory, null);
     }
-    
-    
 
-	@FXML
+    private void loadTree(File dir, TreeItem<String> parent) {
+        TreeItem<String> root = new TreeItem<>(dir.getName());
+        root.setExpanded(true);
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            if (file.isDirectory())
+                loadTree(file, root);
+            else
+                root.getChildren().add(new TreeItem<>(file.getName()));
+        }
+
+        if (parent == null)
+            tree.setRoot(root);
+        else
+            parent.getChildren().add(root);
+
+        root.setExpanded(false);
+    }
+
+    @FXML
     void save(ActionEvent event) {
     }
 
@@ -104,10 +88,6 @@ public class Controller {
         assert create != null : "fx:id=\"create\" was not injected: check your FXML file 'codeeditor.fxml'.";
         assert open != null : "fx:id=\"open\" was not injected: check your FXML file 'codeeditor.fxml'.";
         assert save != null : "fx:id=\"save\" was not injected: check your FXML file 'codeeditor.fxml'.";
-        
-   
-
-
     }
 
 }
