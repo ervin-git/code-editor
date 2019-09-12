ebe02db0b83a68d6e43f51d4ef55b9c286da3d47
Armand Rosario <31975821+armandrosario@users.noreply.github.com>
2019-09-11 20:18:07 -0500

    Update Controller.java

--- src/main/java/com/codeeditor/Controller.java
+++ src/main/java/com/codeeditor/Controller.java
@@ -1,4 +1,20 @@
-package com.codeeditor;
+package application;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class Controller {
+	@FXML
+	public Label label;
+	public MenuItem menuItem;
+	
+	public void initialize() {
+		
+	}
+	@FXML
+	private void changeText(ActionEvent event) {
+		menuItem.setText("Change Text");
+	}
 }
